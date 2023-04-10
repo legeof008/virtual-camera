e0d0a2937e96153180e7f08ca48a2e2cdacac684
Maciej Michalski <legeof008@gmail.com>
2023-04-10 16:36:13 +0200

    feat: finished project

--- src/integration/coversions.rs
+++ src/integration/coversions.rs
@@ -1,7 +1,37 @@
-use cgmath::prelude::*;
-use cgmath::Vector2;
+use cgmath::{vec3, vec4, Matrix4, Vector3, Vector4};
+use std::ops::Mul;
 
 use sdl2::rect::Point;
-pub fn from_vec3(v: Vector2<i32>) -> Point {
-    return Point::new(250, 250);
+
+pub fn get_clipped(
+    point: (i32, i32, i32),
+    global_state_vector: Vector4<f64>,
+    rot_y: Matrix4<f64>,
+    rot_x: Matrix4<f64>,
+    projection_matrix: Matrix4<f64>,
+) -> Vector3<f64> {
+    let rotated_global_state_vector = rot_y.mul(global_state_vector);
+    let vectorised_point = from_edge_tuple(point);
+    let point_rotated_around_y = rot_y.mul(vectorised_point);
+    let projected_point =
+        projection_matrix.mul(rot_x.mul(point_rotated_around_y + rotated_global_state_vector));
+    return clip(projected_point);
+}
+fn from_edge_tuple(v: (i32, i32, i32)) -> Vector4<f64> {
+    return vec4(v.0 as f64, v.1 as f64, v.2 as f64, 1.);
+}
+
+fn clip(v: Vector4<f64>) -> Vector3<f64> {
+    return vec3(v.x / v.w, v.y / v.w, v.z / v.w);
+}
+
+pub fn project_onto(v: Vector3<f64>, height: f64, width: f64, projection_r: f64) -> Point {
+    return Point::new(
+        ((v.x * width) / (projection_r * v.z) + width / 2.) as i32,
+        ((v.y * height) / (projection_r * v.z) + height / 2.) as i32,
+    );
+}
+
+pub fn not_drawable(edge: (Vector3<f64>, Vector3<f64>)) -> bool {
+    return edge.0.z < 1. || edge.1.z < 1.
 }
--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,72 @@ extern crate sdl2;
 
 mod integration;
 
-use cgmath::{perspective, Matrix2, Matrix3, Matrix4, Perspective, Point2, Point3, Rad, Vector2, Vector3, Vector4, Deg};
+use crate::integration::coversions::{get_clipped, not_drawable, project_onto};
+use cgmath::{perspective, vec3, vec4, Deg, Matrix4};
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
-use sdl2::rect::{Point, Rect};
+use std::ops::Mul;
 use std::time::Duration;
 
-fn main() {
-    let v = Vector2::new(1.0, 2.0);
+const MAP_OF_VIEW: [((i32, i32, i32), (i32, i32, i32)); 48] = [
+    ((0, 0, 5), (10, 0, 5)),
+    ((10, 0, 5), (10, 20, 5)),
+    ((0, 20, 5), (10, 20, 5)),
+    ((0, 20, 5), (0, 0, 5)),
+    ((0, 0, 10), (10, 0, 10)),
+    ((10, 0, 10), (10, 20, 10)),
+    ((10, 20, 10), (0, 20, 10)),
+    ((0, 0, 10), (0, 20, 10)),
+    ((0, 20, 5), (0, 20, 10)),
+    ((10, 20, 5), (10, 20, 10)),
+    ((10, 0, 5), (10, 0, 10)),
+    ((0, 0, 5), (0, 0, 10)),
+    ((20, 0, 5), (30, 0, 5)),
+    ((30, 0, 5), (30, 20, 5)),
+    ((20, 20, 5), (30, 20, 5)),
+    ((20, 20, 5), (20, 0, 5)),
+    ((20, 0, 10), (30, 0, 10)),
+    ((30, 0, 10), (30, 20, 10)),
+    ((30, 20, 10), (20, 20, 10)),
+    ((20, 0, 10), (20, 20, 10)),
+    ((20, 20, 5), (20, 20, 10)),
+    ((30, 20, 5), (30, 20, 10)),
+    ((30, 0, 5), (30, 0, 10)),
+    ((20, 0, 5), (20, 0, 10)),
+    ((0, 0, 25), (10, 0, 25)),
+    ((10, 0, 25), (10, 10, 25)),
+    ((0, 10, 25), (10, 10, 25)),
+    ((0, 10, 25), (0, 0, 25)),
+    ((0, 0, 30), (10, 0, 30)),
+    ((10, 0, 30), (10, 10, 30)),
+    ((10, 10, 30), (0, 10, 30)),
+    ((0, 0, 30), (0, 10, 30)),
+    ((0, 10, 25), (0, 10, 30)),
+    ((10, 10, 25), (10, 10, 30)),
+    ((10, 0, 25), (10, 0, 30)),
+    ((0, 0, 25), (0, 0, 30)),
+    ((20, 0, 25), (30, 0, 25)),
+    ((30, 0, 25), (30, 10, 25)),
+    ((20, 10, 25), (30, 10, 25)),
+    ((20, 10, 25), (20, 0, 25)),
+    ((20, 0, 30), (30, 0, 30)),
+    ((30, 0, 30), (30, 10, 30)),
+    ((30, 10, 30), (20, 10, 30)),
+    ((20, 0, 30), (20, 10, 30)),
+    ((20, 10, 25), (20, 10, 30)),
+    ((30, 10, 25), (30, 10, 30)),
+    ((30, 0, 25), (30, 0, 30)),
+    ((20, 0, 25), (20, 0, 30)),
+];
 
+fn main() {
+    let width = 800.;
+    let height = 600.;
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
     let window = video_subsystem
-        .window("vcam", 800, 600)
+        .window("vcam", width as u32, height as u32)
         .position_centered()
         .build()
         .unwrap();
@@ -25,93 +77,85 @@ fn main() {
     canvas.clear();
     canvas.present();
 
-
     let mut event_pump = sdl_context.event_pump().unwrap();
     let mut i = 0;
-    let mut scale = 300;
-    let mut transform_x = 3;
-    let mut transform_y = 3;
-    let mut transform_z = 0;
-    let step = 1;
-    let step_z = 1;
 
-    let view = Matrix4::look_to_rh(Point3::new(4, 3, 3), Vector3::new(0, 0, 0), Vector3::new(0, 1, 0));
-    let proj = perspective( PI, 800. / 600. , 0.1, 100.0);
+    let mut global_state_vector = vec4(3., -2., 5., 0.);
+    let projection_radius = 2.;
+    let mut rotate_y = 0.;
+    let mut rotate_x = 0.;
 
+    let rotation_step = 45.;
+
+    let proj = perspective(Deg(45.), 800. / 600., 0.1, 100.0);
     'game: loop {
         i = (i + 1) % 255;
 
         canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
         canvas.clear();
 
+        let rot_x = Matrix4::from_axis_angle(vec3(1., 0., 0.), Deg(rotate_x));
+        let rot_y = Matrix4::from_axis_angle(vec3(0., 1., 0.), Deg(-(rotate_y)));
+        let rot_transform = Matrix4::from_axis_angle(vec3(0., 1., 0.), Deg(rotate_y));
+
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit { .. } | Event::KeyDown {
+                Event::Quit { .. }
+                | Event::KeyDown {
                     keycode: Some(Keycode::Escape),
                     ..
-                } => { break 'game; }
+                } => {
+                    break 'game;
+                }
                 Event::KeyDown {
-                    keycode: Some(Keycode::Left), ..
-                } => { transform_x -= step }
+                    keycode: Some(Keycode::Left),
+                    ..
+                } => global_state_vector -= rot_transform.mul(vec4(1., 0., 0., 0.)),
                 Event::KeyDown {
-                    keycode: Some(Keycode::Right), ..
-                } => { transform_x += step }
+                    keycode: Some(Keycode::Right),
+                    ..
+                } => global_state_vector += rot_transform.mul(vec4(1., 0., 0., 0.)),
                 Event::KeyDown {
-                    keycode: Some(Keycode::Up), ..
-                } => { transform_z -= step_z }
+                    keycode: Some(Keycode::Up),
+                    ..
+                } => global_state_vector -= rot_transform.mul(vec4(0., 0., 1., 0.)),
                 Event::KeyDown {
-                    keycode: Some(Keycode::Down), ..
-                } => { transform_z += step_z }
-
+                    keycode: Some(Keycode::Down),
+                    ..
+                } => global_state_vector += rot_transform.mul(vec4(0., 0., 1., 0.)),
+                Event::KeyDown {
+                    keycode: Some(Keycode::W),
+                    ..
+                } => rotate_x += rotation_step,
+                Event::KeyDown {
+                    keycode: Some(Keycode::S),
+                    ..
+                } => rotate_x -= rotation_step,
+                Event::KeyDown {
+                    keycode: Some(Keycode::E),
+                    ..
+                } => rotate_y -= rotation_step,
+                Event::KeyDown {
+                    keycode: Some(Keycode::Q),
+                    ..
+                } => rotate_y += rotation_step,
                 _ => {}
             }
         }
         // Here I'll try to get a 3d rectangle to a 2d view
         canvas.set_draw_color(Color::RGB(0, 0, 0));
-        let _p1 = Point3::new(5, 5, 5);
-        let _p2 = Point3::new(0, 5, 5);
-        let _p3 = Point3::new(5, 0, 5);
-        let _p4 = Point3::new(0, 0, 5);
-        let _p5 = Point3::new(5, 0, 10);
-        let _p6 = Point3::new(5, 5, 10);
-        let _p7 = Point3::new(5, 0, 10);
-        let _p8 = Point3::new(0, 0, 10);
 
-        let v1 = ((0, 0, 5), (5, 0, 5));
-        let v2 = ((5, 0, 5), (5, 5, 5));
-        let v3 = ((0, 5, 5), (5, 5, 5));
-        let v4 = ((0, 5, 5), (0, 0, 5));
+        for edge in MAP_OF_VIEW {
+            let p1 = get_clipped(edge.0, global_state_vector, rot_y, rot_x, proj);
+            let p1_projected = project_onto(p1, height, width, projection_radius);
 
-        let v5 = ((0, 0, 10), (5, 0, 10));
-        let v6 = ((5, 0, 10), (5, 5, 10));
-        let v7 = ((5, 5, 10), (0, 5, 10));
-        let v8 = ((0, 0, 10), (0, 5, 10));
+            let p2 = get_clipped(edge.1, global_state_vector, rot_y, rot_x, proj);
+            let p2_projected = project_onto(p2, height, width, projection_radius);
 
-        let v9 = ((0, 5, 5), (0, 5, 10));
-        let v10 = ((5, 5, 5), (5, 5, 10));
-        let v11 = ((5, 0, 5), (5, 0, 10));
-        let v12 = ((0, 0, 5), (0, 0, 10));
-
-        let vertices = [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12];
-
-        'drawing: for vertice in vertices {
-            let z_divider_1 = vertice.0.2 + transform_z;
-            let z_divider_2 = vertice.1.2 + transform_z;
-            if transform_z > z_divider_1 {
+            if not_drawable((p1,p2)) {
                 continue;
             }
-            canvas
-                .draw_line(
-                    Point::new(
-                        (transform_x + vertice.0.0) * scale / z_divider_1,
-                        (transform_y + vertice.0.1) * scale / z_divider_1,
-                    ),
-                    Point::new(
-                        (transform_x + vertice.1.0) * scale / z_divider_2,
-                        (transform_y + vertice.1.1) * scale / z_divider_2,
-                    ),
-                )
-                .unwrap();
+            canvas.draw_line(p1_projected, p2_projected).unwrap();
         }
 
         canvas.present();
